b7b35c2e9e4c28537967aa9bbf736c981726e94f
James Coates <james.coates99@hotmail.com>
2022-10-09 16:42:16 +0100

    Get package published

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <p id="para">This is an example vue component</p>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ExampleComponent',
+};
+</script>
+
+<style scoped>
+#para {
+  background-color: aqua;
+}
+</style>
