da7045aaa01fffe8fe11f413480f29336ee43d85
James Coates <james.coates99@hotmail.com>
2022-10-09 23:20:07 +0100

    Use local package in consuming project

--- consuming-project/src/App.vue
+++ consuming-project/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <ExampleComponent value="This is overridden text" />
+    <ExampleLocalComponent value="This uses the local package" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import { ExampleComponent } from 'example-vue-npm-package';
+import { ExampleComponent as ExampleLocalComponent } from 'local-package';
 
 export default Vue.extend({
   name: 'App',
-  components: { ExampleComponent },
+  components: {
+    ExampleComponent,
+    ExampleLocalComponent,
+  },
 });
 </script>
 
