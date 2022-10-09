5d8a0d3655228a961c15ee86f176706e167419ba
James Coates <james.coates99@hotmail.com>
2022-10-09 21:43:08 +0100

    Use locally published package

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <ExampleComponent />
+    <LocalExampleComponent value="This component uses the local package" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import ExampleComponent from './components/ExampleComponent.vue';
+import { ExampleComponent as LocalExampleComponent } from 'local-package';
 
 export default Vue.extend({
   name: 'App',
   components: {
     ExampleComponent,
+    LocalExampleComponent,
   },
 });
 </script>
